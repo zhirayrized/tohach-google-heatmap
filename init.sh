00a8b920b5ded8d3c1e61ffc70dbeff4d554525e
Zhirayr K. <quicksilverwork@gmail.com>
2015-04-12 18:40:44 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-bundle exec rake db:create db:migrate
-echo "Please, execute ./run.sh"
+bundle install && bundle exec rake db:create db:migrate
+echo "Please execute ./run.sh then upload your first geopoints set"
